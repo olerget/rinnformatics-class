3cb1dce582bf5a1e72df36a9f2ff23d6def94fa6
olerget <merget_severin@web.de>
2025-06-30 20:37:26 +0200

    new file structure

---
+++ alldata/01_mouse_dox_wt/rnaseq/run.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+pwd; hostname; date
+echo "Here we go You've requested $SLURM_CPUS_ON_NODE core."
+
+nextflow run nf-core/rnaseq -r 3.14.0 \
+-resume \
+-profile docker \
+--input samplesheet.csv \
+--outdir /scratch/Shares/rinn/Kit/class_RNASeq/output_dir \
+--reads /scratch/Shares/rinn/ML/mouse_wt_long_timecourse/*{_R1,_R2}.fastq.gz \
+--fasta /scratch/Shares/rinn/ML/DESeq2/GENOMES/M25/GRCm38.p6.genome.fa \
+--gtf /scratch/Shares/rinn/ML/DESeq2/GENOMES/M25/gencode.vM25.annotation.gtf \
+--pseudo_aligner salmon \
+--gencode \
+--email merget_severin@web.de \
+-c nextflow.config
